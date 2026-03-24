4d2b46e1253d4104514c4978d2d6be8d3e3c0f95
Xavier Nayrac <xavier.nayrac@gmail.com>
2026-03-24 21:16:29 +0100

    Improve fish title

--- fish/.config/fish/functions/title.fish
+++ fish/.config/fish/functions/title.fish
@@ -1,5 +1,10 @@
 function title \
   --description 'Change the window title of the terminal'
 
+  if set --query $argv
     echo -en "\033]0;$argv\007"
+  else
+    set t (path basename $PWD)
+    echo -en "\033]0;$t\007"
+  end
 end
